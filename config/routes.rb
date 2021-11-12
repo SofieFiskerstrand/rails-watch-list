e5f698f5958c2a2e5c7c0439326e7e76b581bb5f
Sofie Fiskerstrand <sofiepilskog@gmail.com>
2021-11-12 11:49:32 +0000

    pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
+  # A user can see all the lists
+  resources :lists, only: %i[show index new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
